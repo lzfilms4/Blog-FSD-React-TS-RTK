1b5a58fe56766bfdacc9b5696ec6d6598181d24f
lzfilms4 <lzfilms4@gmail.com>
2023-07-14 14:05:23 +0700

    add page loader, not found page, add error boundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,26 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import Button from 'shared/ui/Button/Button';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+// Компонент для тестирования ErrorBoundary
+const BugButton = () => {
+    const [error, setError] = useState(false);
+    const { t } = useTranslation();
+    const onThrow = () => setError(true);
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+    return (
+        <Button
+            onClick={onThrow}
+        >
+            {t('throw error')}
+        </Button>
+    );
+};
+
+export default BugButton;
