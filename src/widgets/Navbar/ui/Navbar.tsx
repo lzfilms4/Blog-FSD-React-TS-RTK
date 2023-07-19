bf271477cc480918ff75b7dc9e58da5e47c99f07
lzfilms4 <lzfilms4@gmail.com>
2023-07-19 17:17:07 +0700

    update ci

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
@@ -13,7 +12,7 @@ const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                a
+                /
             </div>
         </div>
     );
